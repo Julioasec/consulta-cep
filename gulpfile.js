ed9296b312e9f7a3dee45072aebcb2337e1a7c5c
Julio Silva <ed_julio@outlook.com>
2022-05-19 13:27:28 -0300

    modificado o arquivo index.html e os arquivos sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const watch = require('gulp-watch');
-const cssNano = require('gulp-cssnano');
-const sourceMaps = require('gulp-sourcemaps')
-let isProd;
 
 
 //builda o sass em css
 function sassBuild() {
-
-    let srcUrl = isProd ? './src/css/style.css' : './src/scss/**/*.scss';
-    let destUrl = isProd ? './dist/css': './src/css';
-
-    if(isProd){
-        return gulp.src(srcUrl)
-        .pipe(cssNano({
-                autoprefixer: {
-                    browsers:['>1%','last 2  versions'],
-                    add:true
-                }
-        }))
-        .pipe(gulp.dest(destUrl))
-    }
     
-    return gulp.src(srcUrl)
-    .pipe(sourceMaps.init())
+    return gulp.src('./src/scss/**/*.scss')
     .pipe(sass()
     .on('error', sass.logError))
-    .pipe(sourceMaps.write('.'))
-    .pipe(gulp.dest(destUrl))
+    .pipe(gulp.dest('./src/css'))
    
 }
 
-//modo watch
-
-function watchFiles() {
-    return watch('./src/scss/**/*.scss', exports.buildprod)
-}
 
 
-
-function dev(cb) {
-    isProd = false;
-    cb();
-}
-
-function prod(cb) {
-    isProd = true;
-    cb();
-}
-
-
-
-
-exports.watch = watchFiles;
-exports.buildDev = gulp.series(dev, gulp.parallel(sassBuild))
-exports.buildProd = gulp.series(prod, gulp.parallel(sassBuild))
+exports.default = sassBuild;
