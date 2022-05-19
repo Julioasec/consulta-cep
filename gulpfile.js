36dec432ec25aab39b3ba897a260ebd74fc7ef19
Julio Silva <ed_julio@outlook.com>
2022-05-19 14:57:21 -0300

    criado o soucemaps na tarefa sassBuild

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourceMaps = require('gulp-sourcemaps')
 
 
 //builda o sass em css
 function sassBuild() {
-    
+
     return gulp.src('./src/scss/**/*.scss')
+    .pipe(sourceMaps.init())
     .pipe(sass()
     .on('error', sass.logError))
+    .pipe(sourceMaps.write('.'))
     .pipe(gulp.dest('./src/css'))
    
 }
